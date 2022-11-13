bc5cb0107c28cbe6b74a9282e6577f17f7f18880
Benjamin <bennhyon@gmail.com>
2022-11-13 11:06:28 +0100

    Added utils class with Logger Fixed default image not being set on cats with no image

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import discord
 import random
 import json
-
+from enum import Enum
+from utils import *
 #Globals
 CATS_JSON_PATH = "data/cats/cats.json"
 CAT_SPRITES_PATH = "resources/Cat_Sprites/"
+Logger.DEBUG_LEVEL = 1
+
 # Enums
-class Rarity :
+class Rarity(Enum) :
     COMMON = 1
-    RARE = 2
-    SUPER_RARE = 3
-    UBER_RARE = 4
-    LEGEND_RARE = 5
+    SPECIAL = 2
+    RARE = 3
+    SUPER_RARE = 4
+    UBER_RARE = 5
+    LEGEND_RARE = 6
 
 # Classes
 class Cat :
@@ -23,11 +27,23 @@ class Cat :
 
     def __init__(self, name :str, rarity : Rarity, power : int, health : int ,image : str = "unknown.webp",):
         self.name = name
-        self.image = image
+        try:
+            with open(CAT_SPRITES_PATH + image, "r") as f:
+                f.close()
+                self.image = image
+        except:
+            Logger.log(3, f"Couldn't set {self.name} image, setting default")
+            self.image = "unknown.webp"
+
         self.rarity = rarity
         self.power = power
         self.health = health
+    
+    def getCatImageFilePath(self):
+        return CAT_SPRITES_PATH + self.image
 
+    def __str__(self) -> str:
+        return f"{self.name} , Image:{self.image} , Rarity:{self.rarity} , Power:{self.power} , Health:{self.health}"
 
 # Factories
 class CatFactory:
@@ -52,34 +68,51 @@ if __name__ == "__main__":
     intents.message_content = True
 
     client = discord.Client(intents = intents)
-
+    main_channel : discord.TextChannel
     cats_list_json : dict = json.loads(open(CATS_JSON_PATH, 'r').read())
     
     def summon_cat(cat : Cat):
         pass
 
-    async def try_summon_cat_in_channel(channel : discord.TextChannel):
-        catToSummon = CatFactory.createCatFromJsonString(cats_list_json[ random.choice( list(cats_list_json) ) ])
-
-        with open(f"{CAT_SPRITES_PATH + catToSummon.image}", "rb") as f:
-            await channel.send(f"A wild {catToSummon.name} appeared ! ", file=discord.File(f))
+    async def try_summon_cat_in_channel(channel : discord.TextChannel, catName = None):
+        catToSummon : Cat = None
+        try:
+            if catName is not None:
+                catToSummon = CatFactory.createCatFromJsonString(cats_list_json[catName])
+            else:
+                catToSummon = CatFactory.createCatFromJsonString(cats_list_json[ random.choice( list(cats_list_json) ) ])
+        except:
+            catToSummon = CatFactory.createCatFromJsonString(cats_list_json[ random.choice( list(cats_list_json) ) ])
+        await channel.send(f"A wild **{str.upper(Rarity(catToSummon.rarity).name)}** {catToSummon.name} appeared ! ", file=discord.File( catToSummon.getCatImageFilePath() ) )
 
         return
+
+    async def sendErrorInChat(channel : discord.TextChannel, errorMessage):
+        await channel.send(errorMessage)
+    
 
     # -- Events
     @client.event
     async def on_ready():
         print(f'We have logged in as {client.user}')
+        main_channel = client.get_channel(1041000719467151392)
+        if Logger.DEBUG_LEVEL >= 2:
+            await main_channel.send("I'm alive !")
+        
 
     @client.event
     async def on_message(message : discord.Message):
         if message.author == client.user:
             return
-        
 
-        if message.content == "$summon":
-            await try_summon_cat_in_channel(message.channel)
-
+        if message.content.startswith("$summon"):
+            args : list = message.content.split(" ")[1:]
+            cat = ' '.join(args)
+            try:
+                await try_summon_cat_in_channel(message.channel, cat)
+            except Exception as e:
+                await sendErrorInChat(message.channel, f"Couldn't summon a cat: \n {e}")
+            
 
 
 
---
+++ utils.py
@@ -0,0 +1,15 @@
+
+class Logger:
+    """Custom logger class
+    """
+    DEBUG_LEVEL : int = 0
+    
+    @staticmethod
+    def log( *args, debug_level : int = 1):
+        """Logs a message if the input debug level (default 1) is higher than the gobal Debug level
+
+        Args:
+            debug_level (int, optional): Debug level. Defaults to 1.
+        """
+        if  debug_level >= Logger.DEBUG_LEVEL:
+            print(args)
